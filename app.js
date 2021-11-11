7292070532efd2a436afafa42def937a5067c45e
vinay564 <38555014+vinay564@users.noreply.github.com>
2021-11-10 20:30:12 -0600

    made changes app.js

--- app.js
+++ app.js
@@ -4,11 +4,55 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
+const connectionString = process.env.MONGO_CON;
+mongoose = require("mongoose");
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var lampRouter = require('./routes/Lamp');
 var addmodsRouter = require('./routes/addmods');
 var selectRouter = require('./routes/selector');
+var Lamp = require("./models/lamp");
+
+// We can seed the collection if needed on
+//server start
+//server start
+async function recreateDB() {
+  // Delete everything
+  await Lamp.deleteMany();
+  let instance1 = new Lamp({
+    lamp_type: "Arc Lamps",
+    duration: 5,
+    cost: 34,
+  });
+  let instance2 = new Lamp({
+    lamp_type: "Tripod Lamps",
+    duration: 6,
+    cost: 35,
+  });
+  let instance3 = new Lamp({
+    lamp_type: "Swing Arm Lamps",
+    duration: 7,
+    cost: 36,
+  });
+  instance1.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("First object saved");
+  });
+  instance2.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("Second object saved");
+  });
+  instance3.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("Third object saved");
+  });
+}
 
 var app = express();
 

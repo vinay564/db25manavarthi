3b4c91fee87aaecf64740a3485a1297d30ccb6b8
vinay564 <38555014+vinay564@users.noreply.github.com>
2021-11-19 00:12:02 -0600

    lab 13 completed

--- app.js
+++ app.js
@@ -3,6 +3,27 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongoose = require('mongoose');
+var Costume = require("./models/costumes");
+
+passport.use(new LocalStrategy(function(username, password, done) {
+  Account.findOne({ username: username }, function (err, user) {
+    if (err) { 
+      return done(err); 
+    }
+    if (!user) {
+      return done(null, false, { 
+        message: 'Incorrect username.' 
+      });
+    }
+    if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+    }
+    return done(null, user);
+  });
+}));
 var mongoose = require('mongoose');
 var Costumes = require("./models/costumes");
 
@@ -70,12 +91,41 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+
+
+
+
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({
   extended: false
 }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+ }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/costumes', costumesRouter);
+app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
+app.use('/', resourceRouter);
+
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
